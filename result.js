d6e07d350f2581630bd4e3ef882afe36f6c57a2b
aswanth-mc <mcaswnth5@gmail.com>
2026-04-07 21:31:18 +0530

    feat : add profile featch

--- result.js
+++ result.js
@@ -1,33 +1,25 @@
 const params = new URLSearchParams(window.location.search);
 const username = params.get('user');
 
-if (!username) {
-    card.innerHTML = "no username provided";
-}
-else{
-    fetch(`https://api.github.com/users/${username}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('User not found');
-        }
-        return response.json();
-    })
-    .then(data => {
-        displayUserInfo(data);
-    })
-    .catch(error => {
-        card.innerHTML = error.message;
-    });
-}
+const profile=document.querySelector('.profile');
 
-function displayUserInfo(user) {
-    const card = document.querySelector('.card');
-    card.innerHTML = `
-        <img src="${user.avatar_url}" alt="${user.login}'s avatar">
-        <h2>${user.name || user.login}</h2>
-        <p>${user.bio || 'No bio available'}</p>
-        <p>Public Repositories: ${user.public_repos}</p>
-        <p>Followers: ${user.followers}</p>
-        <p>Following: ${user.following}</p>
-    `;
+if (!username) {
+    profile.innerHTML = 'No username provided in the URL.';
+}
+else {
+    fetch(`https://api.github.com/users/${username}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('user not found');
+            }
+            return response.json();
+        })
+        .then(user=>{
+            displayUserInfo(user);
+        })
+        .catch(error => {
+            profile.innerHTML = error.message;
+        });
+
+
 }
